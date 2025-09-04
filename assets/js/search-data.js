218d81026fe9502b706ffc405b6297478512c821
zhangxuan1918 <zhangxuan1918@gmail.com>
2025-09-04 14:55:20 +0000

    Deploying to gh-pages from @ zhangxuan1918/zhangxuan1918.github.io@13de266aade1a1c4414a1b3d6448bd6c698825d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-grpo-experiments",
+        },{id: "post-moe",
+        
+          title: "MoE",
+        
+        description: "Mixture of Experts",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/moe/";
+          
+        },
+      },{id: "post-grpo-experiments",
         
           title: "GRPO Experiments",
         
