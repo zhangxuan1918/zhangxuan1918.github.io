599ab32b6711e0d8435e56cf4f7a6f8882feda0d
zhangxuan1918 <zhangxuan1918@gmail.com>
2026-05-06 10:39:17 +0000

    Deploying to gh-pages from @ zhangxuan1918/zhangxuan1918.github.io@15c9d05ea3ff69a271e82cae009000c7e3d36773 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-moe",
+        },{id: "post-steepest-descent-or-ascent",
+        
+          title: "Steepest descent or ascent",
+        
+        description: "Optimization",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/steepest/";
+          
+        },
+      },{id: "post-moe",
         
           title: "MoE",
         
